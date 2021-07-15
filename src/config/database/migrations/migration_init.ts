70441d1ca15a3e7c58120dbdc1ad538ae92a883c
Edmilson Andrade <edmilisonwork@gmail.com>
2021-07-15 09:53:43 +0100

    changed name convection for migrations

--- src/config/database/migrations/migration_init.ts
+++ src/config/database/migrations/migration_init.ts
@@ -1,6 +1,6 @@
 import pool 			from '../connection';
 import mysql_migrations	from 'mysql-migrations';
 
-mysql_migrations.init(pool, `${__dirname}/migrations`, () => {
+mysql_migrations.init(pool, `${__dirname}/migrations_list`, () => {
 	console.log("Migrations process done!");
 });
