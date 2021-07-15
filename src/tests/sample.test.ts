5e4252e4b9af16f9fefc5bd5f46eb756f847dd5b
Edmilson Andrade <edmilisonwork@gmail.com>
2021-07-15 13:22:40 +0100

    Removed unnecessary tests

--- src/tests/sample.test.ts
+++ src/tests/sample.test.ts
@@ -1,6 +1,5 @@
 import func from '../api/toTest';
 import { expect }	from 'chai';
-import classT from '../api/classexample';
 
 describe('Sample Test', () => {
 
@@ -10,15 +9,5 @@ describe('Sample Test', () => {
 
 	it('should test a function', () => {
 		expect(func(1)).to.equal(1);
-	});
-});
-
-
-describe('Sample Test', () => {
-
-	const thisclass = new classT();
-
-	it('should test a function', () => {
-		expect(thisclass.firstM()).to.equal(true);
 	});
 });
