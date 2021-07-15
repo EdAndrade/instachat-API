df765dd308494d4044f965a141536b11943b1c5d
Edmilson Andrade <edmilisonwork@gmail.com>
2021-07-15 11:23:18 +0100

    migrations setup done

--- src/config/database/migrations/migration_init.ts
+++ src/config/database/migrations/migration_init.ts
@@ -2,5 +2,5 @@ import pool 			from '../connection';
 import mysql_migrations	from 'mysql-migrations';
 
 mysql_migrations.init(pool, `${__dirname}/migrations_list`, () => {
-	console.log("Migration process done!");
+	console.log("Migration process finished!");
 });
