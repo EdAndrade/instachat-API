a24bb5c1995c306ccc50cf9e856afc62f44a154c
Edmilson Andrade <edmilisonwork@gmail.com>
2021-07-16 11:14:54 +0100

    Resolving hash generator test

---
+++ src/tests/api/Services/GenerateHashService.test.ts
@@ -0,0 +1,10 @@
+import hashGenerator from '../../../api/Services/GenerateHashService';
+
+describe('Testing hash generator', () => {
+
+	test('Should return a hash string', () => {
+
+		const hash = hashGenerator('test');
+		expect(hash).toBe(typeof String);
+	});
+});
