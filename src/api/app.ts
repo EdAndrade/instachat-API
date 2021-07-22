ecca5eb3a31845689b670f9ab696ef1f1d966266
Edmilson Andrade <edmilisonwork@gmail.com>
2021-07-22 12:10:33 +0100

    Routed start chat controller

--- src/api/app.ts
+++ src/api/app.ts
@@ -3,6 +3,7 @@ import cors					from 'cors';
 
 //======ROUTES
 import generateChatRoute	from './Routes/ChatGeneratorRoute';
+import startChatRoute		from './Routes/ChatStarterRoute';
 
 const app 			= express();
 const routerRoot	= '/api/';
@@ -11,5 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(`${routerRoot}chat`, generateChatRoute);
+app.use(`${routerRoot}chat`, startChatRoute);
 
 export default app;
