bf6214e55dfc2ceb0251320d279eab99b1058e2b
Edmilson Andrade <edmilisonwork@gmail.com>
2021-10-20 21:47:24 +0100

    fechada versao 1

--- src/api/Controllers/ChatStarterControllter.ts
+++ src/api/Controllers/ChatStarterControllter.ts
@@ -24,15 +24,11 @@ export default class ChatStarterController {
 
 				if(chatResponse.result){
 					const processResult: ProcessResult = this.handleChatRooms.HandleProcess(chatResponse.result);
-
+					return response.status(200).json(processResult);
+				}else{
+					return response.sendStatus(404);
 				}
 				
-				return chatResponse.result ? response.status(200).json({
-					chatCode	: chatResponse.result.chatCode,
-					timeToInit	: chatResponse.result.timeToInit,
-					dateToInit	: chatResponse.result.dateToInit,
-				}) : response.sendStatus(404);
-
 			}else{
 				return response.sendStatus(500);
 			}
@@ -70,25 +66,71 @@ class HandleChatRooms {
 			});
 		}
 
-		const chat					= this.getChatByChatCode(chatRoom.chatCode);
-		const newElement: string 	= this.generateNewUserName(chat.elementsQtd);
-
-		return this.addNewElementToChatRoom(chatRoom.chatCode, newElement);
+		const chat										= this.getChatByChatCode(chatRoom.chatCode);
+		const newElement: string 						= this.generateNewUserName(chat.elementsQtd);
+		const newElementAdditionResult: ProcessResult	= this.addNewElementToChatRoom(chatRoom.chatCode, newElement);
+		const chatValidation: ProcessResult				= this.isChatTimeAndDateValid(chatRoom.timeToInit, chatRoom.dateToInit);
+		
+		return chatValidation.success ? newElementAdditionResult : chatValidation;
 	}
 
-	private verifyActivityChatTimeAndDate(chatTime: string, chatDate: string){
+	private isChatTimeAndDateValid(chatTime: string, chatDate: string): ProcessResult{
 
 		const date: Date = new Date();
 		const currentHour: number = date.getHours();
 		const currentMinutes: number = date.getMinutes();
-		const currentDay: number = date.getDay();
+		const currentDay: number = date.getDate();
 
 		const chatTimeSplited: Array<string> = chatTime.split(':');
 		const chatDateSplited: Array<string> = chatDate.split('-');
 		const chatHour = Number(chatTimeSplited[0]);
 		const chatMinutes = Number(chatTimeSplited[1]);
+		const chatDay = Number(chatDateSplited[0]);
 
-		const chatInitTime: string = date.setHours()
+		const expiredChatResponse: ProcessResult = { success: false, message: 'Chat expirado!' };
+		const validChatResponse: ProcessResult = { success: true, message: 'Chat válido!' };
+		const minutesChatRangeVerification: boolean = ((chatMinutes >= 55) && (chatMinutes <= 59) && (chatMinutes - (60+currentMinutes) < 5))
+
+		if( chatDay === currentDay){
+
+			if(chatHour === currentHour){
+
+				if( (currentMinutes - chatMinutes) < 0){
+
+					return {
+						success: false,
+						message: 'Sala de chat ainda não foi iniciada'
+					};
+					
+				}else if(currentMinutes - chatMinutes >= 5){
+					return expiredChatResponse;
+				}
+
+			}else if(chatHour === (currentHour-1)){
+
+				if(minutesChatRangeVerification){
+					return validChatResponse;
+				}
+			}
+
+		}else if( chatDay === (currentDay-1)){
+
+			if( (chatHour === 23) && minutesChatRangeVerification){
+				return validChatResponse;
+			}else{
+				return expiredChatResponse;
+			}
+
+		}else if(currentDay > chatDay){
+			
+			return {
+				success: false,
+				message: 'Sala de chat ainda não foi iniciada'
+			};
+
+		}else{
+			return expiredChatResponse;
+		}
 	}
 
 	private checkIfChatAlreadyExists(chatCode: string): boolean{
@@ -100,15 +142,11 @@ class HandleChatRooms {
 		return this.activeChatRooms.find( chat => chatCode === chat.chatCode);
 	}
 
-	// private addNewChatRoom(chatCode: string, userQtd: number){
-
-	// }
-
 	private generateNewUserName(chatQtd: number): string{
-
+		return `#@42${chatQtd}`
 	}
 
-	private addNewElementToChatRoom(chatCode: string, newElement: string): boolean{
+	private addNewElementToChatRoom(chatCode: string, newElement: string): ProcessResult{
 		let isDone = false;
 
 		this.activeChatRooms.forEach( chatRoom => {
@@ -118,7 +156,10 @@ class HandleChatRooms {
 			}
 		});
 
-		return isDone;
+		return {
+			success: isDone,
+			message: isDone ? 'User added to chat' : 'Chat is already full!'
+		};
 	}
 
 }
