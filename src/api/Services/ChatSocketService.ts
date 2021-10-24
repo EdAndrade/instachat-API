1a847d32ba559236d3b43e6fc956a68619d9a289
Edmilson Andrade <edmilisonwork@gmail.com>
2021-10-24 16:36:06 +0100

    created socket service

---
+++ src/api/Services/ChatSocketService.ts
@@ -0,0 +1,23 @@
+import ws, { WebSocketServer } from 'ws';
+
+export default class ChatSocket {
+
+	private readonly _socket: WebSocketServer;
+
+	constructor(socket: WebSocketServer){
+		this._socket = socket;
+		this.handleSocket(this._socket);
+	}
+
+	handleSocket(socket: WebSocketServer): void {
+
+		socket.on("connection", ws => {
+			console.log("new clientConnected");
+
+			socket.on("message", data => {
+
+				ws.send("recivied");
+			});
+		});
+	}
+}
