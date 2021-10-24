0411068371f42cae0f0b3ed9a36adf8a9d394655
Edmilson Andrade <edmilisonwork@gmail.com>
2021-10-24 14:37:47 +0100

    fixed sql migration script

--- src/api/Data/Database/migrations/migrations_list/1635080866799_create_chat_table.js
+++ src/api/Data/Database/migrations/migrations_list/1635080866799_create_chat_table.js
@@ -1,10 +1,10 @@
 module.exports = {
 	"up":
-		`CREATE TABLE IF NOT EXITS chats(
+		`CREATE TABLE IF NOT EXISTS chats(
 			id INT AUTO_INCREMENT PRIMARY KEY,
 			code VARCHAR(100),
 			users_qty INT,
-			chat_name VARCHAR(128),
+			chat_name VARCHAR(128)
 		)`,
 
 	"down": "DROP TABLE chats"
